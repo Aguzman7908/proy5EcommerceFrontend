d5f97cf61cdc79e631b3af297d3eef96764b8a7e
Angel Guzman <angel.guzman@mx.bellota.com>
2023-04-27 19:32:39 -0600

    Vamos actualizar

--- controllers/products.controllers.js
+++ controllers/products.controllers.js
@@ -13,7 +13,7 @@ const getProducts = async (req, res) => {
 const getProductIndividual =async (req, res) => {
     try{
         const sku = req.params.sku
-        const products = await Product.find({sku})
+        const products = await Product.findOne({sku})
         res.json({data:products,})
     } catch (error){
         res.status (400).json({msg:'Hubo un error explorando los datos'})
